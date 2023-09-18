2ebc4c311859417823637e2fa6d597b8920459a5
ADRIEN NGASSAM <adrientchana2@gmail.com>
2023-09-18 08:27:44 +0200

    ajout de la fonction filtre

--- src/index.js
+++ src/index.js
@@ -17,54 +17,16 @@ const app = initializeApp(firebaseConfig);
 //Initialize database firestore
 const db = getFirestore(app);
 
-const conducteurs = collection(db, "Chauffeurs");
+const conducteursRef = collection(db, "Chauffeurs");
+let conducteurs = [];
 
-getDocs(conducteurs).then((snapshot) => {
-    let conducteurs = [];
+getDocs(conducteursRef).then((snapshot) => {
     snapshot.docs.forEach((doc) => {
         conducteurs.push((doc.data()));
     });
     console.log(conducteurs);
 
-    for(let i=0;i<=conducteurs.length;i++){
-    const article = conducteurs[i];
-const part=document.createElement("div");
-
-    const DateDepartElement = document.createElement("p");
-    DateDepartElement.innerText = `Date de Depart du conducteur : ${article.DateDepart}`;
-    const DateArrivieeElement = document.createElement("p");
-    DateArrivieeElement.innerText = `Date d arrivee du conducteur d'arrivee : ${article.DateArrivee}`;
-    const HeureDepartElement = document.createElement("p");
-    HeureDepartElement.innerText = `Heure de depart du conducteur : ${ article.HeureDepart}`;
-    const HeureArriveeElement=document.createElement("p");
-    HeureArriveeElement.innerText=` Heure d arrivee du conducteur : ${article.HeureArrivee}`;
-    const LieuDepartElement=document.createElement("p");
-    LieuDepartElement.innerText=`Lieu de depart du conducteur : ${article.LieuDepart}`;
-    const LieuArriveeElement=document.createElement("p"); 
-    LieuArriveeElement.innerText=`Lieu d'arrivee du conducteur : ${article.LieuArrivee}`;
-    const PrixPlaceElement=document.createElement("p");
-    PrixPlaceElement.innerText=`Prix d'une place : ${article.PrixPlace}`;
-    const numElement=document.createElement("p");
-    numElement.innerText=`Nombre de places disponibles : ${article.numero}`;
-    const telephoneElement=document.createElement("p");
-    telephoneElement.innerText=`Numero de telephone : ${article.telephone}` ;
-    const matriculeElement = document.createElement("p");
-    matriculeElement.innerText= `Numero de telephone : ${article.matricule}` ;
-
-    //Rattachement de nos balises au DOM
-    const ElementRecherche = document.querySelector(".search");
-    ElementRecherche.appendChild(part);
-    part.appendChild(DateDepartElement);
-    part.appendChild(DateArrivieeElement);
-    part.appendChild(HeureDepartElement);
-    part.appendChild(HeureArriveeElement);
-    part.appendChild(LieuDepartElement);
-    part.appendChild(LieuArriveeElement);
-    part.appendChild(PrixPlaceElement);
-    part.appendChild(numElement);
-    part.appendChild(telephoneElement);
-    part.appendChild(matriculeElement);
-    };
+    affichage(conducteurs)
 })
 
 
@@ -75,9 +37,8 @@ if (addConductorForm) {
     addConductorForm.addEventListener('submit', (e) => {
         e.preventDefault();
 
-
         //Ajouter un nouveau document avec un id genere
-        addDoc(conducteurs, {
+        addDoc(conducteursRef, {
             DateDepart: addConductorForm.Date_Depart.value,
             DateArrivee: addConductorForm.Date_arriver.value,
             HeureDepart: addConductorForm.Heure_Depart.value,
@@ -93,6 +54,74 @@ if (addConductorForm) {
     })
 }
 
+function affichage(conducteurs) {
+
+    if (conducteurs.length == 0)
+        return;
+
+    const ElementRecherche = document.querySelector("#conducteur-content");
+    ElementRecherche.innerHTML = ''
 
 
+    for (let i = 0; i < conducteurs.length; i++) {
+        const article = conducteurs[i];
+        const part = document.createElement("div");
+        part.innerHTML = ""
+        const DateDepartElement = document.createElement("p");
+        DateDepartElement.innerText = `Date de Depart du conducteur : ${article.DateDepart}`;
+        const DateArrivieeElement = document.createElement("p");
+        DateArrivieeElement.innerText = `Date d arrivee du conducteur d'arrivee : ${article.DateArrivee}`;
+        const HeureDepartElement = document.createElement("p");
+        HeureDepartElement.innerText = `Heure de depart du conducteur : ${article.HeureDepart}`;
+        const HeureArriveeElement = document.createElement("p");
+        HeureArriveeElement.innerText = ` Heure d arrivee du conducteur : ${article.HeureArrivee}`;
+        const LieuDepartElement = document.createElement("p");
+        LieuDepartElement.innerText = `Lieu de depart du conducteur : ${article.LieuDepart}`;
+        const LieuArriveeElement = document.createElement("p");
+        LieuArriveeElement.innerText = `Lieu d'arrivee du conducteur : ${article.LieuArrivee}`;
+        const PrixPlaceElement = document.createElement("p");
+        PrixPlaceElement.innerText = `Prix d'une place : ${article.PrixPlace}`;
+        const numElement = document.createElement("p");
+        numElement.innerText = `Nombre de places disponibles : ${article.numero}`;
+        const telephoneElement = document.createElement("p");
+        telephoneElement.innerText = `Numero de telephone : ${article.telephone}`;
+        const matriculeElement = document.createElement("p");
+        matriculeElement.innerText = `Numero de telephone : ${article.matricule}`;
 
+        //Rattachement de nos balises au DOM
+        ElementRecherche.appendChild(part);
+        part.appendChild(DateDepartElement);
+        part.appendChild(DateArrivieeElement);
+        part.appendChild(HeureDepartElement);
+        part.appendChild(HeureArriveeElement);
+        part.appendChild(LieuDepartElement);
+        part.appendChild(LieuArriveeElement);
+        part.appendChild(PrixPlaceElement);
+        part.appendChild(numElement);
+        part.appendChild(telephoneElement);
+        part.appendChild(matriculeElement);
+    };
+}
+
+
+function search(lieu_arriver, lieu_Depart) {
+    if (conducteurs != []) {
+        let searchTab = conducteurs.filter(conducteur => conducteur.LieuDepart.trim().toLowerCase().includes(lieu_Depart.trim().toLowerCase()) && conducteur.LieuArrivee.trim().toLowerCase().includes(lieu_arriver.trim().toLowerCase()))
+        console.log({ searchTab })
+        affichage(searchTab)
+    }
+
+}
+
+function btnclick() {
+    console.log(conducteurs)
+    const lieu_arrive = document.getElementById('Lieu-arriver').value
+    const lieu_Depart = document.getElementById('Lieu-Depart').value
+    search(lieu_arrive, lieu_Depart )
+}
+
+const searchConducteurBtn = document.querySelector("#btn-conducteur-search");
+
+searchConducteurBtn.addEventListener('click', (e) => {
+    btnclick()
+})
